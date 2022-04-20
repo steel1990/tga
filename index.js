6a1ee197fe6ed52fd4ae7ead8262b89806db7789
steel1990 <lbbsteel@gmail.com>
2022-04-20 17:51:28 +0800

    support color map type tga image

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const debug = require('debug')('TGA');
 
+const tempColor = new Uint8Array(4);
+
 // doc: http://paulbourke.net/dataformats/tga/
 class TGA {
     constructor(buffer, opt) {
@@ -10,7 +12,7 @@ class TGA {
     }
     static createTgaBuffer(width, height, pixels, dontFlipY) {
         debug('createTgaBuffer');
-        var buffer = Buffer.alloc(18 + pixels.length);
+        const buffer = Buffer.alloc(18 + pixels.length);
         // write header
         buffer.writeInt8(0, 0);
         buffer.writeInt8(0, 1);
@@ -25,10 +27,10 @@ class TGA {
         buffer.writeInt8(32, 16);
         buffer.writeInt8(0, 17);
 
-        var offset = 18;
-        for (var i = 0; i < height; i++) {
-            for (var j = 0; j < width; j++) {
-                var idx = ((dontFlipY ? i : height - i - 1) * width + j) * 4;
+        let offset = 18;
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                const idx = ((dontFlipY ? i : height - i - 1) * width + j) * 4;
                 buffer.writeUInt8(pixels[idx + 2], offset++);    // b
                 buffer.writeUInt8(pixels[idx + 1], offset++);    // g
                 buffer.writeUInt8(pixels[idx], offset++);        // r
@@ -40,13 +42,13 @@ class TGA {
     }
     static getHeader(buffer) {
         debug('getHeader');
-        var header = {};
+        const header = {};
         header.idlength = buffer.readInt8(0);
-        header.colourMapType = buffer.readInt8(1);
+        header.colorMapType = buffer.readInt8(1);
         header.dataTypeCode = buffer.readInt8(2);
-        header.colourMapOrigin = buffer.readInt16LE(3);
-        header.colourMapLength = buffer.readInt16LE(5);
-        header.colourMapDepth = buffer.readInt8(7);
+        header.colorMapOrigin = buffer.readInt16LE(3);
+        header.colorMapLength = buffer.readInt16LE(5);
+        header.colorMapDepth = buffer.readInt8(7);
         header.xOrigin = buffer.readInt16LE(8);
         header.yOrigin = buffer.readInt16LE(10);
         header.width = buffer.readInt16LE(12);
@@ -63,10 +65,11 @@ class TGA {
             return;
         }
         this.readPixels();
+        this.fixForAlpha();
     }
     readHeader() {
         debug('readHeader');
-        var header = TGA.getHeader(this.buffer);
+        const header = TGA.getHeader(this.buffer);
         this.width = header.width;
         this.height = header.height;
         this.bytesPerPixel = header.bytesPerPixel = header.bitsPerPixel / 8;
@@ -76,78 +79,129 @@ class TGA {
     }
     check() {
         debug('check tga file');
-        var header = this.header;
+        const header = this.header;
         /* What can we handle */
-        if (header.dataTypeCode != 2 && header.dataTypeCode != 10) {
+        if (![1, 2, 10].includes(header.dataTypeCode)) {
             console.error('Can only handle image type 2 and 10');
             return false;
         }
-        if (header.bitsPerPixel != 16 && 
-            header.bitsPerPixel != 24 && header.bitsPerPixel != 32) {
+        if (![8, 16, 24, 32].includes(header.bitsPerPixel)) {
             console.error('Can only handle pixel depths of 16, 24, and 32');
             return false;
         }
-        if (header.colourMapType != 0 && header.colourMapType != 1) {
-            console.error('Can only handle colour map types of 0 and 1');
+        if (header.colorMapType != 0 && header.colorMapType != 1) {
+            console.error('Can only handle color map types of 0 and 1');
             return false;
         }
         return true;
     }
-    addPixel(arr, offset, idx) {
+    setPixel(idx, color) {
         if (this.isFlipY) {
-            var y = this.height - 1 - Math.floor(idx / this.width);
+            const y = this.height - 1 - Math.floor(idx / this.width);
             idx = y * this.width + idx % this.width;
         }
-        idx *= 4;
-        var count = this.bytesPerPixel;
-        var r = 255;
-        var g = 255;
-        var b = 255;
-        var a = 255;
-        if (count === 3 || count === 4) {
-            r = arr[offset + 2];
-            g = arr[offset + 1];
-            b = arr[offset];
-            a = count === 4 ? arr[offset + 3] : 255;
-        } else if (count === 2) {
-            r = (arr[offset + 1] & 0x7c) << 1;
-            g = ((arr[offset + 1] & 0x03) << 6) | ((arr[offset] & 0xe0) >> 2);
-            b = (arr[offset] & 0x1f) << 3;
-            a = (arr[offset + 1] & 0x80);
-        } else {
-            console.error('cant transform to Pixel');
-        }
+        this.pixels.set(color, idx * 4);
+    }
 
-        this.pixels[idx] = r;
-        this.pixels[idx + 1] = g;
-        this.pixels[idx + 2] = b;
-        this.pixels[idx + 3] = a;
+    readColor(arr, offset, bytesPerPixel) {
+        if (bytesPerPixel === 3 || bytesPerPixel === 4) {
+            tempColor[0] = arr[offset + 2];
+            tempColor[1] = arr[offset + 1];
+            tempColor[2] = arr[offset];
+            tempColor[3] = bytesPerPixel === 4 ? arr[offset + 3] : 255;
+        } else if (bytesPerPixel === 2) {
+            tempColor[0] = (arr[offset + 1] & 0x7c) << 1;
+            tempColor[1] = ((arr[offset + 1] & 0x03) << 6) | ((arr[offset] & 0xe0) >> 2);
+            tempColor[2] = (arr[offset] & 0x1f) << 3;
+            tempColor[3] = (arr[offset + 1] & 0x80);
+        } else {
+            console.error('Can\'t read color with bytesPerPixel=%s', bytesPerPixel);
+        }
+        return tempColor;
+    }
+
+    readColorMap(data, offset) {
+        debug('readColorMap');
+        const colorMapLength = this.header.colorMapLength;
+        const colorMapBytesPerPixel = this.header.colorMapDepth / 8;
+        const colorMap = new Uint8Array(colorMapLength * 4);
+        for (let i = 0; i < colorMapLength; i++) {
+            const color = this.readColor(data, offset, colorMapBytesPerPixel);
+            colorMap.set(color, i * 4);
+            offset += colorMapBytesPerPixel;
+        }
+        return {
+            origin: this.header.colorMapOrigin,
+            colorMap,
+            bytes: colorMapLength * colorMapBytesPerPixel,
+            offset,
+        };
     }
     readPixels() {
         debug('readPixels');
-        var header = this.header;
-        var bytesPerPixel = header.bytesPerPixel;
-        var pixelCount = header.width * header.height;
-        var data = new Uint8Array(this.buffer);
-        this.pixels = new Uint8Array(pixelCount * 4);
-        var offset = 18;
+        const header = this.header;
+        const pixelCount = header.width * header.height;
+        const data = new Uint8Array(this.buffer);
+        let offset = 18 + this.header.idlength; // jump for Image Identification Field
 
-        for (var i = 0; i < pixelCount; i++) {
+        this.pixels = new Uint8Array(pixelCount * 4);
+
+        let colorMap;
+
+        if (header.colorMapType === 1) {
+            const colorMapInfo = this.readColorMap(data, offset);
+            colorMap = colorMapInfo.colorMap;
+            offset = colorMapInfo.offset;
+        }
+
+        for (let i = 0; i < pixelCount; i++) {
             if (header.dataTypeCode === 2) {
-                this.addPixel(data, offset, i);
+                const color = this.readColor(data, offset, this.bytesPerPixel);
+                offset += this.bytesPerPixel;
+                this.setPixel(i, color);
             } else if (header.dataTypeCode === 10) {
-                var flag = data[offset++];
-                var count = flag & 0x7f;
-                var isRLEChunk = flag & 0x80;
-                this.addPixel(data, offset, i);
-                for (var j = 0; j < count; j++) {
+                const flag = data[offset++];
+                const count = flag & 0x7f;
+                const isRLEChunk = flag & 0x80;
+                let color = this.readColor(data, offset, this.bytesPerPixel);
+                offset += this.bytesPerPixel;
+                this.setPixel(i, color);
+                for (let j = 0; j < count; j++) {
                     if (!isRLEChunk) {
+                        color = this.readColor(data, offset, this.bytesPerPixel);
                         offset += this.bytesPerPixel;
                     }
-                    this.addPixel(data, offset, ++i);
+                    this.setPixel(++i, color);
+                }
+            } else if (header.dataTypeCode === 1) {
+                let index = data[offset];
+                if (this.bytesPerPixel === 2) {
+                    index = this.buffer.readUInt16LE(offset);
+                } else if (this.bytesPerPixel === 4) {
+                    index = this.buffer.readUInt32LE(offset);
+                }
+                const color = colorMap.subarray(index * 4, index * 4 + 4);
+                this.setPixel(i, color);
+                offset += this.bytesPerPixel;
+            }
+        }
+    }
+
+    fixForAlpha() {
+        debug('fixForAlpha');
+        let alpha0Count = 0;
+        for (let i = this.pixels.length - 1; i > 0; i -= 4) {
+            if (!this.pixels[i]) {
+                alpha0Count++;
+            }
+        }
+        if (alpha0Count > this.pixels.length / 4 * .98) {
+            // 98% pixels are transparent, convert all alpha to 255
+            for (let i = this.pixels.length - 1; i > 0; i -= 4) {
+                if (!this.pixels[i]) {
+                    this.pixels[i] = 255;
                 }
             }
-            offset += this.bytesPerPixel;
         }
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,22 +1,49 @@
 var fs = require('fs');
+const path = require('path');
 var TGA = require('../index');
 
-var fileBuffer = fs.readFileSync('./test.tga');
+function convertToPNG(src, dest) {
+    var fileBuffer = fs.readFileSync(src);
 
-console.time('parse tga');
-var tga = new TGA(fileBuffer);
-console.timeEnd('parse tga');
-console.log('tga info:', tga.width, tga.height);
+    console.time('parse tga');
+    var tga = new TGA(fileBuffer);
+    console.timeEnd('parse tga');
+    console.log('tga info:', tga.width, tga.height);
 
-var buf = TGA.createTgaBuffer(tga.width, tga.height, tga.pixels);
-fs.writeFileSync('./out.tga', buf);
+    var buf = TGA.createTgaBuffer(tga.width, tga.height, tga.pixels);
+    fs.writeFileSync('./out.tga', buf);
 
-console.time('topng');
-var PNG = require('pngjs').PNG;
-var png = new PNG({
-    width: tga.width,
-    height: tga.height
-});
-png.data = tga.pixels;
-png.pack().pipe(fs.createWriteStream('./test.png'));
-console.timeEnd('topng');
+    console.time('topng');
+    var PNG = require('pngjs').PNG;
+    var png = new PNG({
+        width: tga.width,
+        height: tga.height
+    });
+    png.data = tga.pixels;
+    png.pack().pipe(fs.createWriteStream(dest));
+    console.timeEnd('topng');
+}
+
+const list = [
+    'flag_b16.tga',
+    'flag_b24.tga',
+    'flag_b32.tga',
+    'flag_t16.tga',
+    'flag_t24.tga',
+    'flag_t32.tga',
+    'xing_b16.tga',
+    'xing_b24.tga',
+    'xing_b32.tga',
+    'xing_t16.tga',
+    'xing_t24.tga',
+    'xing_t32.tga',
+    'shuttle.tga',
+    'fern.tga',
+    'football_seal.tga',
+    'earth.tga',
+    'test.tga',
+];
+
+list.forEach((file) => {
+    convertToPNG(path.join(__dirname, file), path.join(__dirname, file + '.png'));
+})
